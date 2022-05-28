27656bf4e966401b91d9eddae2702b944759c21e
tsuzat <aloksingh772002@gmail.com>
2022-05-28 12:30:46 +0530

    Small changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
       title: "WhatApp",
       home: Home(),
     );
